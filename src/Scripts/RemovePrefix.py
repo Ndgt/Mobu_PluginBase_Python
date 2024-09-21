4b3d6e04ada017dcfe465596599b8d46a41e109f
Ndgt <nadesubber2@gmail.com>
2024-09-21 12:03:01 +0900

    不要箇所を削除

--- src/Scripts/RemovePrefix.py
+++ src/Scripts/RemovePrefix.py
@@ -4,22 +4,15 @@
 
 from pyfbsdk import*
 
-def RemovePrefix(mlist:FBModelList, prefix:str):
-    for model in mlist:
-        if model.Name.startswith(prefix):
-            model.Name = model.Name.replace(prefix,"")
+mlist = FBModelList()
+FBGetSelectedModels(mlist)
 
-def main():
-    mlist = FBModelList()
-    FBGetSelectedModels(mlist)
-    
-    if len(mlist) == 0:
-        FBMessageBox("Caution","You must select some models","OK")
-    
-    else:
-        prefix = FBMessageBoxGetUserValue("Message", "prefix to omit", None,
-                                       FBPopupInputType.kFBPopupString, "execute")
-        RemovePrefix(mlist, prefix[1])        
-        
-if __name__ in ("__main__", "builtins"):
-    main()
+if len(mlist) == 0:
+    FBMessageBox("Caution","You must select some models","OK")
+
+else:
+    prefix = FBMessageBoxGetUserValue("Message", "prefix to omit", None,
+                                    FBPopupInputType.kFBPopupString, "execute")
+    for model in mlist:
+        if model.Name.startswith(prefix[1]):
+            model.Name = model.Name.replace(prefix[1],"") 
