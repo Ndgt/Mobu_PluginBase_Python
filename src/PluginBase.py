e7b99a4817d4de91ff504208207d752ea8342253
Ndgt <nadesubber2@gmail.com>
2024-11-14 09:24:36 +0900

    update comments

--- src/PluginBase.py
+++ src/PluginBase.py
@@ -5,18 +5,18 @@ from pathlib import Path
 from pyfbsdk import FBGetMainWindow, FBApplication
 
 
-# MotionBuilder 2025 向け
+# for MotionBuilder 2025
 try:
     from PySide6.QtWidgets import QMainWindow, QMenuBar
     from shiboken6 import wrapInstance
 
-# for MotionBuilder 2024 向け
+# for MotionBuilder 2024
 except:
     from PySide2.QtWidgets import QMainWindow, QMenuBar
     from shiboken2 import wrapInstance
 
 
-# メニューバーの取得
+# Get Menubar
 def getMenubar() -> QMenuBar:
     pMainW = FBGetMainWindow()
     if pMainW:
@@ -24,20 +24,18 @@ def getMenubar() -> QMenuBar:
         menubar = MainW.menuWidget().children()[1]
         return menubar
 
-# メニューバーへのメニューの追加
+# Add Menu in the Menubar
 def AddMenu(mbar : QMenuBar):
     if mbar is not None:
         PluginBaseDir = Path(__file__).resolve().parent
-
-        # 独自のツールおよびスクリプトを格納する先のディレクトリ
         tools_dir   = PluginBaseDir / "Tools"
         scripts_dir = PluginBaseDir / "Scripts"
 
-        # 空のメニューを追加
+        # Add Empty Menu
         tmenu = mbar.addMenu("Tools")
         smenu = mbar.addMenu("Scripts")
 
-        # ツールの場合
+        # if tools
         for tools_filepath in tools_dir.iterdir():
             if str(tools_filepath).endswith(".py"):
                 module_name = tools_filepath.stem
@@ -47,11 +45,12 @@ def AddMenu(mbar : QMenuBar):
                 t = tmenu.addAction(module_name)
                 t.triggered.connect(module.ActivateTool)
         
-        # スクリプトの場合
+        # if scripts
         for script_filepath in scripts_dir.iterdir():
             if str(script_filepath).endswith(".py"):
                 s = smenu.addAction(script_filepath.stem)
                 s.triggered.connect(lambda check=False, sp = str(script_filepath) : FBApplication().ExecuteScript(sp))
+
 
 if __name__ in ("__main__", "builtins"):
     menubar = getMenubar()
